183ceec98ef6e802899fb865c065861cb0bfd96c
Michael Zavarella <mzavarella30@gmail.com>
2025-11-04 12:51:59 -0500

    Implement ccwc line count

--- solutions/ccwc/main.go
+++ solutions/ccwc/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	countBytes := flag.Bool("c", false, "Count bytes")
+	countLines := flag.Bool("l", false, "Count lines")
 
 	flag.Parse()
 
@@ -29,4 +30,23 @@ func main() {
 		byteCount := len(data)
 		fmt.Printf("%d %s\n", byteCount, filename)
 	}
+
+	if *countLines {
+		lineCount := countLinesInData(data)
+		fmt.Printf("%d %s\n", lineCount, filename)
+	}
+}
+
+func countLinesInData(data []byte) int {
+	count := 0
+	for _, b := range data {
+		if b == '\n' {
+			count++
+		}
+	}
+	// If the file does not end with a newline, count the last line
+	if len(data) > 0 && data[len(data)-1] != '\n' {
+		count++
+	}
+	return count
 }
